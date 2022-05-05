3a31710fd7bc96429c0cf104d140b2bc80dee21b
Pierce <piercerhamstine@gmail.com>
2022-05-05 13:37:18 -0500

    File rename

--- include/Graphs/graph.hpp
+++ include/Graphs/graph.hpp
@@ -1,9 +1,21 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 
+#include "vector"
+
+struct Vertex
+{
+    int vertexID;
+    int vertexWeight;
+};
+
 class Graph
 {
-
+public:
+    void AddVertex(int weight);
+    void AddEdge(int fromVert, int toVert);
+private:
+    std::vector<Vertex> vertices;
 };
 
 #endif
